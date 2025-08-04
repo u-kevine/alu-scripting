2a42627e5497589b7c176d795ef6dae722d4843e
u-kevine <k.uwisanga@alustudent.com>
2025-08-04 15:25:07 +0000

    api

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-""" Get the titles of the first 10 hot posts from a subreddit."""
+"""" Top Ten Limit"""
 import requests
 
 
 def top_ten(subreddit):
-    headers = {'User-Agent': 'Comfortable_Net_1144'}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=True)
-    print("Status: {}".format(response.status_code))
+    """"top ten"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
 
-    if response.status_code == 200:
-        json_data = response.json()
-        posts = json_data.get('data', {}).get('children', [])
-        for post in posts[:10]:
-            print(post.get('data', {}).get('title'))
-    else:
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
+
+    if res.status_code != 200:
         print(None)
+    else:
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]#!/usr/bin/python3
+"""Get the titles of the first 10 hot posts for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+
+    """Print titles of top 10 hot posts for a subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None, end='')
+        return
+    posts = response.json().get('data', {}).get('children', [])
+    for i in range(min(10, len(posts))):
+        print(posts[i].get('data', {}).get('title', '').strip())
